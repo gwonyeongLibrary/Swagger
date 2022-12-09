a845657a64f724f48f83b0d5169cc30bf6baeea3
Gwonyeong <rnjsdud980@naver.com>
2022-12-09 16:10:15 +0900

    Add: post_add router

--- routers/calc/calc.controller.js
+++ routers/calc/calc.controller.js
@@ -4,6 +4,17 @@ const add = (req, res, next) => {
   res.send(String(result));
 };
 
+const post_add = (req, res, next) => {
+  try {
+    const { num1, num2, num3 } = req.body;
+    const result = parseInt(num1) + parseInt(num2) + parseInt(num3);
+    res.send(String(result));
+  } catch (err) {
+    res.status(400).send("error!");
+  }
+};
+
 module.exports = {
   add,
+  post_add,
 };
--- routers/calc/index.js
+++ routers/calc/index.js
@@ -2,5 +2,6 @@ const calcRouter = require("express").Router();
 const calc_controller = require("./calc.controller");
 
 calcRouter.get("/add", calc_controller.add);
+calcRouter.post("/add", calc_controller.post_add);
 
 module.exports = calcRouter;
